a9b30463615107ad8ff83bd344c0c973ca3e2494
ss2567 <shiyaosun@dhcp-rhodes-455.redrover.cornell.edu>
2016-02-21 20:50:49 -0500

    PS01Q1

---
+++ PS2/TEAM34PS01Q1.m
@@ -0,0 +1,12 @@
+% Find the smallest distance h from pi/4 that has a relative error smaller
+% than 10^-6
+h = 0.2;                                    %start with h=0.2
+actual = cos(pi/4);                         %compute the actual value
+computed = (sin(pi/4+h)-sin(pi/4))/h;       %use the forward finite difference formula
+error = abs((computed - actual)/actual);
+while error > 1*10^(-6)
+    h=h/10;
+    computed = (sin(pi/4+h)-sin(pi/4))/h;
+    error = abs((computed - actual)/actual);
+end
+disp(h)
