f27ed21152944cb0bd1337e4725550d681830662
sjt85 <sjt85@cornell.edu>
2016-02-28 21:46:21 -0500

    Clean up in code, working on report

--- PS02/TEAM34PS02Q2.m
+++ PS02/TEAM34PS02Q2.m
@@ -8,26 +8,24 @@ df = @(x) pi*(sec(pi*x))^2 -1;
 
 % Get bisect data
 [mass blah ea iter bsect]=bisect(fx,.4,.48,.001,100);
-bsect1 = cell2mat(bsect(2:16,:));
+bsect1 = cell2mat(bsect(2:16,:)); %Convert to numeric values
 
 % Get Newton-Raphson data
 [root,ea,iter,nr] = newtraph(fx,df,0.48,.0001,25);
-nr1 = cell2mat(nr(2:7,:));
+nr1 = cell2mat(nr(2:7,:)); %Convert to numeric values
 
 % Get secant data
 [y,sc] = secant(fx,0.54,.48,.0001);
-sc1 = cell2mat(sc(2:10,:));
+sc1 = cell2mat(sc(2:10,:)); %Convert to numeric values
 
 close all
 
 figure(1)
-%hold on
-%scatter(bsect1(1:15,1),bsect1(1:15,5),nr1(1:6,1),nr1(1:6,4),sc1(1:9,1),sc1(1:9,4))
 semilogy(bsect1(1:15,1),bsect1(1:15,5),'-o')
 hold on
 plot(nr1(1:6,1),nr1(1:6,4),'-o')
 plot(sc1(1:9,1),sc1(1:9,4),'-o')
 legend('Bisect','Newton-Raphson','Secant')
 xlabel('Iteration')
-ylabel('log_{10}Error')
+ylabel('log_{10}(Approx Error)')
 hold off
