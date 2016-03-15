7ad48e40462a3a4f927ac41068d3926505c49521
ss2567 <shiyaosun@dhcp-rhodes-2417.redrover.cornell.edu>
2016-03-15 12:46:53 -0400

    ..

--- PS03/GaussionElimination.m
+++ PS03/GaussionElimination.m
@@ -5,14 +5,13 @@ time_GE = [];
 for k = 1:length(N)
     TotalTime = 0;
     for j = 1:100
-    A = rand(N(k),N(k));
-    b = rand(N(k),1);
-    tic
-    x = A \ b;
-    time_elapsed = toc;
-    TotalTime = TotalTime + time_elapsed;
+        A = rand(N(k),N(k));
+        b = rand(N(k),1);
+        tic
+        x = A \ b;
+        time_elapsed = toc;
+        TotalTime = TotalTime + time_elapsed;
     end
     Ave = TotalTime / 100;
     time_GE = [time_GE Ave];
 end
-    
