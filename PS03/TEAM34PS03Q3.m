bf3d2fb3c9352929fc61efddfc7829e6fb7575b9
sjt85 <sjt85@cornell.edu>
2016-03-15 23:34:48 -0400

    Adding framework for problem 3

---
+++ PS03/TEAM34PS03Q3.m
@@ -0,0 +1,19 @@
+% Forms the tridiagonal matrix A
+% Uses the previously-developed LU factorization and substitution code 
+% to solve this matrix
+% Finds the associated error norm
+% Solves using the Thomas algorithm and compares
+
+
+
+
+function [A b] = formA(N)
+% Creates the tridiagonal matrix A found using the heat equation
+% Creates the RHS vector b from the sine function
+
+
+
+
+end
+
+
