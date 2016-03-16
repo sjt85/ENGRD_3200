2ed78c5844adc0450b059ca163a8ba60311b2a26
ss2567 <shiyaosun@dhcp-rhodes-767.redrover.cornell.edu>
2016-03-15 23:49:14 -0400

    …

--- PS03/Q2cd.m
+++ PS03/Q2cd.m
@@ -3,6 +3,7 @@
 % versus N
 % Plot the ratio of the time using Gaussian Elimination divided by the time
 % using LU factorization versus N
+
 N = [10 50 100 500 1000];
 time_BF = [];
 time_GE = [];
@@ -10,9 +11,9 @@ Ratio = [];
 for k = 1:length(N)
     TotalTime1 = 0;
     TotalTime2 = 0;
-    A = rand(N(k),N(k));
-    b = rand(N(k),1);
-    for j = 1:100
+    A = rand(N(k),N(k));    % Generate a random N * N matrix
+    b = rand(N(k),1);       % Generate a random vector
+    for j = 1:100           % Solve this linear system for 100 times
         tic
         x2 = A \ b;
         time_elapsed1 = toc;
@@ -23,12 +24,12 @@ for k = 1:length(N)
         time_elapsed2 = toc;
         TotalTime2 = TotalTime2 + time_elapsed2;
     end
-    AveGE = TotalTime1 / 100;
-    time_GE = [time_GE AveGE];
-    AveBF = TotalTime2 / 100;
-    time_BF = [time_BF AveBF];
-    ratio = AveGE / AveBF;
-    Ratio = [Ratio ratio];
+    AveGE = TotalTime1 / 100;     % Compute the average time for GE
+    time_GE = [time_GE AveGE];    % Store the average time using GE method
+    AveBF = TotalTime2 / 100;     % Compute the average time for BF
+    time_BF = [time_BF AveBF];    % Store the average time using LU method
+    ratio = AveGE / AveBF;        % Compute the ratio
+    Ratio = [Ratio ratio];        % Store the ratio into this vector
 end
 
 % Part (c) of Question 2
@@ -45,5 +46,5 @@ hold off
 figure (2)
 plot (N, Ratio)
 xlabel('Size of the matrix (N by N)')
-ylabel('Ratio of time_GE divided by time_BF')
+ylabel('Ratio of time GE divided by time BF')
 title ('Ratio of time used vs N')
