c27d1e48a05e7bca8ee985e3f59a2b8ff9233fe5
sjt85 <sjt85@cornell.edu>
2016-03-16 00:15:41 -0400

    Incremental update on question 3

--- PS03/TEAM34PS03Q3.m
+++ PS03/TEAM34PS03Q3.m
@@ -7,10 +7,33 @@
 
 
 
-function [A b] = formA(N)
+function [A b x] = formA(N)
 % Creates the tridiagonal matrix A found using the heat equation
 % Creates the RHS vector b from the sine function
+% Returns the exact solution as a vector x
+% N is the number of nodes
 
+A = zeros(N-1,N-1);  % Initialize matrix with all zeros
+b = zeros(N-1);      % Initialize RHS vector
+x = zeros(N-1);      % Initialize exact solution
+h = 1/N;
+
+% Form the diagonal
+for i = 1 : N-1 
+    A(i,i) = 2;
+end
+
+% Form the subdiagonal and superdiagonal
+for i = 1 : N-2
+    A(i+1,i) = -1;   % Subdiagonal
+    A(i,i+1) = -1;   % Superdiagonal
+end
+
+% Form RHS vector and exact solution
+for i = 1 : N-1
+    b(i) = h^2 * sin(pi*h*i);
+    x(i) = sin(pi*h*i)/pi^2;
+end
 
 
 
