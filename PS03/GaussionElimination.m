ba968300792c74bca6a230d6545303716bb35462
ss2567 <shiyaosun@dhcp-rhodes-767.redrover.cornell.edu>
2016-03-15 23:32:41 -0400

    ..

--- PS03/GaussionElimination.m
+++ PS03/GaussionElimination.m
@@ -1,17 +1,18 @@
 % The code calculates the time it takes to solve Ax = b using Gaussian
 % Elimination
+
 N = [10 50 100 500 1000];
 time_GE = [];
 for k = 1:length(N)
     TotalTime = 0;
-    for j = 1:100
-        A = rand(N(k),N(k));
-        b = rand(N(k),1);
+    A = rand (N(k),N(k));     % Generate a random N * N matrix
+    b = rand(N(k),1);         % Generate a random vector
+    for j = 1:100             % Solve this linear system for 100 times
         tic
         x = A \ b;
         time_elapsed = toc;
         TotalTime = TotalTime + time_elapsed;
     end
-    Ave = TotalTime / 100;
-    time_GE = [time_GE Ave];
+    Ave = TotalTime / 100;    % Compute the average time
+    time_GE = [time_GE Ave];  % Store the average time in this vector
 end
