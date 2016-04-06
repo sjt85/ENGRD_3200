d6bc5302518d4acbe4dc43ead26f78db8ef0c96f
ss2567 <shiyaosun@dhcp-gs-2745.redrover.cornell.edu>
2016-04-06 01:17:23 -0400

    start on Q3

--- PS05/TEAM34PS05Q3.m
+++ PS05/TEAM34PS05Q3.m
@@ -2,10 +2,13 @@ function TEAM34PS05Q3
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 Days = zeros (12,1);
+Augsix = 7*30+6;
 for k = 1:12
-    Days = [Days 
+    Days(k) = 30*(k-1)+15;
+end
 Rad = [144; 188; 245; 311; 351; 359; 308; 287; 260; 211; 159; 131];
-
+figure
+scatter(Days,Rad)
 
 end
 
