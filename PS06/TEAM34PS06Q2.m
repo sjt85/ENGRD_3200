9d07f53812107158acf103337bd54dff6062a0c0
sjt85 <sjt85@cornell.edu>
2016-04-18 17:56:32 -0400

    More work on 2a

--- PS06/TEAM34PS06Q2.m
+++ PS06/TEAM34PS06Q2.m
@@ -2,6 +2,7 @@ function [fout] = TEAM34PS06Q2
 % Sets up the problem
 % Calls subroutine to compute the lagrange IP
 
+
 xdata = [5.88; 12.3; 27.3; 45.8; 69.6; 100];  %phi
 f_i = [0.995; 0.98; 0.92; 0.83; 0.726; 0.617]; %c_p
 n = 5;
--- PS06/interphomecooked.m
+++ PS06/interphomecooked.m
@@ -1,4 +1,27 @@
 function fout = interphomecooked(xdata,f,n,xout)
+% Computes and plots the Lagrange form of the global interpolating
+% polynomial
+% Computes Lagrange interpolant functions for an arbitrary dataset
+% Plots L_N,i for i = 0,1,2 and i = 3,4,5
+% Plots the global interpolating polynomial along with original data
+% Performs a second run, sampling at xout and returns xdata
+
+
+% Set up spacing for use with the plots to have 200 points
+x_points = linspace(xdata(1),xdata(length(xdata)),200);
+
+% Evaluate those Lagrange interpolant functions at the x_points
+% Set up a matrix to contain these values, where the column vector
+% L(:,j+1) corresponds to L_n,j
+
+L_matrix = zeros(length(x_points),n+1);
+
+for j = 0:n
+   for k = 1:length(x_points)
+       L_matrix(x_points(k),j+1) = Lagrange(xdata,x_points(k),j);
+   end
+end
+
 
 
 end
@@ -6,33 +29,21 @@ end
 
 
 
-function yint = Lagrange(x,y,xx)
-% Lagrange: Lagrange interpolating polynomial
-% Taken from textbook Figure 17.9
-% yint = Lagrange(x,y,xx): Uses an (n - 1)-order
-% Lagrange interpolating polynomial based on n data points
-% to determine a value of the dependent variable (yint) at
-% a given value of the independent variable, xx.
-% input:
-% x = independent variable
-% y = dependent variable
-% xx = value of independent variable at which the
-% interpolation is calculated
-% output:
-% yint = interpolated value of dependent variable
+function L = Lagrange(x_vect,x_new,j)
+% Computes the value of the Lagrange interpolant term L_n,j at x_new 
+% based off of the values in x_vect
 
-n = length(x);
-if length(y)~=n, error('x and y must be same length'); end
-s = 0;
+n = length(x_vect);
+num = 1;
+den = 1;
+
 for i = 1:n
-    product = y(i);
-    for j = 1:n
-        if i ~= j
-            product = product*(xx-x(j))/(x(i)-x(j));
-        end
+    if i ~= j
+        num = num*(x_new - x_vect(i));
+        den = den*(x_vect(j) - x_vect(i));
     end
-    s = s+product;
 end
-yint = s;
+
+L = num/den;
 
 end
