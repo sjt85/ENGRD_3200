4d0552eb493f607b46d932f28e3eaa0a335be7e4
sjt85 <sjt85@cornell.edu>
2016-04-18 23:03:40 -0400

    Making pretty pictures

--- PS06/interphomecooked.m
+++ PS06/interphomecooked.m
@@ -6,6 +6,7 @@ function fout = interphomecooked(xdata,f,n,xout)
 % Plots the global interpolating polynomial along with original data
 % Performs a second run, sampling at xout and returns xdata
 
+fout = 1;
 
 % Set up spacing for use with the plots to have 200 points
 x_points = linspace(xdata(1),xdata(length(xdata)),200);
@@ -16,31 +17,79 @@ x_points = linspace(xdata(1),xdata(length(xdata)),200);
 
 L_matrix = zeros(length(x_points),n+1);
 
-for j = 0:n
+for j = 0:n    % Evaluate one L_n,i at a time
    for k = 1:length(x_points)
-       L_matrix(x_points(k),j+1) = Lagrange(xdata,x_points(k),j);
+       L_matrix(k,j+1) = Lagrange(xdata,x_points(k),j,n);
    end
 end
 
 
+% Construct the final polynomial!
+P = zeros(length(x_points),1);
+for i = 0:n
+    P = P + f(i+1) .* L_matrix(:,i+1);
+end
+
+close all
+figure
+% Enlarge figure to full screen.
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
+
+
+ah1 = subplot(3,1,1);
+plot(x_points,L_matrix(:,1),x_points,L_matrix(:,2),x_points,L_matrix(:,3))
+legend('L_{5,0}','L_{5,1}','L_{5,2}','Location','NorthEastOutside')
+ylabel({'Interpolant';'Functions'})
+
+ah2 = subplot(3,1,2);
+plot(x_points,L_matrix(:,4),x_points,L_matrix(:,5),x_points,L_matrix(:,6))
+legend('L_{5,3}','L_{5,4}','L_{5,5}','Location','NorthEastOutside')
+ylabel({'Interpolant';'Functions'})
+
+ah3 = subplot(3,1,3);
+hold on
+plot(x_points,P)
+scatter(xdata,f)
+box on
+hold off
+legend('P_5','Tabulated values','Location','NorthEastOutside')
+xlabel('Mole Percentage \phi [%]')
+ylabel({'Heat Capacity';'c_p [cal/g ^oC]'})
+
+% find current position [x,y,width,height]
+pos3 = get(ah3,'Position');
+pos2 = get(ah2,'Position');
+pos1 = get(ah1,'Position');
+
+% set width of second axes equal to first
+pos1(3) = pos3(3);
+pos2(3) = pos3(3);
+set(ah1,'Position',pos1)
+set(ah2,'Position',pos2)
+
+% turn off axes
+set(ah1,'XTickLabel','')
+set(ah2,'XTickLabel','')
+
+
 
 end
 
 
 
 
-function L = Lagrange(x_vect,x_new,j)
+function L = Lagrange(x_vect,x_new,j,n)
 % Computes the value of the Lagrange interpolant term L_n,j at x_new 
 % based off of the values in x_vect
 
-n = length(x_vect);
+% n = length(x_vect);
 num = 1;
 den = 1;
 
-for i = 1:n
+for i = 0:n  % Take the product of all these
     if i ~= j
-        num = num*(x_new - x_vect(i));
-        den = den*(x_vect(j) - x_vect(i));
+        num = num*(x_new - x_vect(i+1));
+        den = den*(x_vect(j+1) - x_vect(i+1)); %Remember index starts at 1
     end
 end
 
