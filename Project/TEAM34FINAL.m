b52bf27d1224b46944a306607be00b96992a26bc
sjt85 <sjt85@cornell.edu>
2016-05-01 21:15:53 -0400

    Files from blackboard and master function

---
+++ Project/TEAM34FINAL.m
@@ -0,0 +1,17 @@
+function [ ] = TEAM34FINAL( )
+% Master function for ENGRD 3200 Final Project
+% Is broken into 3 parts:
+%   1)  Quarter Car Model
+%   2)  Simulation
+%   3)  Data Analysis
+
+
+%%% Part 1:  Quarter Car Model %%%
+
+% Perform least squares fit on experimental spring and dashpot data
+
+
+
+
+
+end
