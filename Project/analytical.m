b6da5414fb968047fbf731e699a2bb19c6162ef6
sjt85 <sjt85@cornell.edu>
2016-05-09 16:31:53 -0400

    Switched to trying a transfer function

--- Project/analytical.m
+++ Project/analytical.m
@@ -21,7 +21,7 @@ Eq2 = laplace(f2(t),t,s);
 
 
 % Replace the strings identifying laplace transforms with sym objects
-syms X Y
+syms X Y Q
 Eq1 = subs(Eq1,{laplace(x(t), t, s),laplace(y(t), t, s)},{X,Y});
 Eq2 = subs(Eq2,{laplace(x(t), t, s),laplace(y(t), t, s)},{X,Y});
 
@@ -30,10 +30,13 @@ Eq2 = subs(Eq2,{x(0),y(0),'D(x)(0)','D(y)(0)'},{0,0,0,0})
 
 % Replace input with its laplace transform derived by hand
 
-Eq1 = subs(Eq1,{laplace(q(t), t, s)},...
-    {(A*omega*exp(-s*T) * (exp(-s*T)+1))/(s^2 + omega^2)});
-Eq2 = subs(Eq2,{laplace(q(t), t, s)},...
-    {(A*omega*exp(-s*T) * (exp(-s*T)+1))/(s^2 + omega^2)});
+%Eq1 = subs(Eq1,{laplace(q(t), t, s)},...
+    %{(A*omega*exp(-s*T) * (exp(-s*T)+1))/(s^2 + omega^2)});
+%Eq2 = subs(Eq2,{laplace(q(t), t, s)},...
+    %{(A*omega*exp(-s*T) * (exp(-s*T)+1))/(s^2 + omega^2)});
+    
+Eq1 = subs(Eq1,{laplace(q(t), t, s)},{Q});
+Eq2 = subs(Eq2,{laplace(q(t), t, s)},{Q});
 
 Eq1 = subs(Eq1,{q(0)},{0})
 Eq2 = subs(Eq2,{q(0)},{0})
@@ -48,10 +51,43 @@ Y = simplify(Y);
 pretty(X)
 pretty(Y)
 
-% Invert the laplace transform
+%%% Put together the transfer functions %%%
 
-syms x1(t) y1(t)
-x1(t) = ilaplace(X, s, t)
-y1(t) = ilaplace(Y, s, t)
+% Need constants first
+ms = 236.12;
+mu = 23.61;
+kt = 181818.88;
+ct = 13.854;
+ks = 34967;
+cs = 905.2896;
+
+% Input transfer function of sprung mass X
+
+Xtf = tf([-ct*cs, cs*kt-ct*ks, kt*ks],[ms*mu, cs*ms-ct*ms+cs*mu, ...
+    ks*ms+kt*ms+ks*mu-cs*ct, cs*kt-ct*ks, ks*kt]);
+
+
+
+% % Substitute in actual values for our constants
+% 
+% X = subs(X,{k,m,c,omega,A,mu,kt,ct,T},...
+%     {34967,236.12,905.2896,6.7128,.1,23.61,181818.88,13.854,.468})
+% 
+% Y = subs(Y,{k,m,c,omega,A,mu,kt,ct,T},...
+%     {34967,236.12,905.2896,6.7128,.1,23.61,181818.88,13.854,.468})
+% 
+% % Invert the laplace transform
+% 
+% syms x1(t) y1(t)
+% x1(t) = ilaplace(X, s, t);
+% y1(t) = ilaplace(Y, s, t);
+% 
+% simplify(x1(t));
+% simplify(y1(t));
+
+
+
+
+
 
 
