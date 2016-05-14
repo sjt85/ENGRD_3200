a543befa1cc7404ed1ee1406fd89c66ccfd32585
sjt85 <sjt85@cornell.edu>
2016-05-14 16:40:37 -0400

    Changed indices to match nomenclature

--- Project/ForwardEuler.m
+++ Project/ForwardEuler.m
@@ -13,10 +13,12 @@ t = linspace(0,tspan,num_steps);
 y = zeros(num_steps,4);
 y(1,:) = y0;  % Initial conditions
 
-for i = 2:num_steps
-    % Evaluate y vector at current time
-    slopes = transpose(dydt(t(i-1),y(i-1,:)));
-    y(i,:) = y(i-1,:) + (t(i)-t(i-1))*slopes;  % Evaluate from previous timestep
+for i = 1:num_steps-1
+    % Evaluate y vector for the next time
+    slopes = transpose(dydt(t(i),y(i,:)));
+    % Evaluate new values based on those from previous timestep
+    % Use special h formula to allow for nonuniform timesteps
+    y(i+1,:) = y(i,:) + (t(i+1)-t(i))*slopes;
 end
 
 
